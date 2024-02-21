f960b6226388994700b55b24fa69f14900cd8edd
hitbheda <zayoxop666@gmail.com>
2024-02-21 16:49:07 +0530

    Here We Go

---
+++ Practicle4/A/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form method='get'>
+    <input type="text" name="text" placeholder="Enter text:"/>
+    <button type='submit'>Submit</button>
+  </form>
+  <?php
+    // $text = "hi bro";
+    if(isset($_GET['text'])){
+      $text = $_GET['text'];
+      $charCount = strlen($text);
+      $wordCount = 0;
+      for($i = 0; $i < $charCount; $i++){
+        if($text[$i] == " "){
+          $wordCount++;
+          $charCount--;
+        }
+      }
+      $wordCount++;
+      echo "String :" . $text . "<br>";
+      echo "Word count: " . $wordCount;
+      echo "<br>";
+      echo "Character count: " . $charCount;
+  }
+  ?>
+</body>
+</html>
---
+++ Practicle4/B/index.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>PHP Shorting</title>
+  </head>
+  <body>
+    <?php 
+      $ar = array(4,2,6,1,8,3,5,7);
+      $temp = array();
+      for($i = 0; $i < count($ar); $i++){
+        for($j = $i + 1; $j < count($ar); $j++){
+          if($ar[$i] > $ar[$j]){
+            $temp = $ar[$i];
+            $ar[$i] = $ar[$j];
+            $ar[$j] = $temp;
+          }
+        }
+      }
+    print_r($ar);
+    ?> 
+
+ 
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
