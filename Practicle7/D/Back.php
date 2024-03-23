10ba32c229175cad58a727d6f1f8fb1577b4bccc
hitbheda <zayoxop666@gmail.com>
2024-03-23 11:16:21 +0530

    Houii

---
+++ Practicle7/D/Back.php
@@ -0,0 +1,7 @@
+<?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            echo "Name =".$_POST['name'].'<br/>';
+            echo "Email =".$_POST['email'].'<br/>';
+            echo "Enrollment =".$_POST['enrollment'].'<br/>';
+        }
+    
---
+++ Practicle7/D/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Details Form</title>
+</head>
+<body>
+    <form action="Back.php" method="post">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br>
+        <label for="enrollment">Enrollment:</label><br>
+        <input type="number" id="enrollment" name="enrollment"><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+</body>
+</html>
