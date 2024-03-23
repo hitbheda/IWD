205bc93b33fcecc320dc70cee0bd02e9d4b058d5
hitbheda <zayoxop666@gmail.com>
2024-03-23 10:34:19 +0530

    Hiii

--- Practicle7/B/Back.php
+++ Practicle7/B/Back.php
@@ -2,12 +2,9 @@
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             echo "Name =".$_POST['name'].'<br/>';
             echo "Email =".$_POST['email'].'<br/>';
+            echo "Role =".$_POST['role'].'<br/>';
             echo "Date Of Birth =".$_POST['dob'].'<br/>';
             echo "Gender =".$_POST['gender'].'<br/>';
             echo "Phone =".$_POST['phone'].'<br/>';
         }
     
-
-
-
-        
